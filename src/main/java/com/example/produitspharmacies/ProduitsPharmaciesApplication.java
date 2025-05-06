54ec64cb84c96be9ed517f07b1fc2fb6602ad527
AyaAbrada <abradaaya2@gmail.com>
2025-05-06 17:54:07 +0200

    Créer entity

---
+++ src/main/java/com/example/produitspharmacies/ProduitsPharmaciesApplication.java
@@ -0,0 +1,13 @@
+package com.example.produitspharmacies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitsPharmaciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProduitsPharmaciesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/produitspharmacies/ProduitsPharmaciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.produitspharmacies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitsPharmaciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
